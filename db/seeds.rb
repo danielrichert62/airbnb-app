f1caf92723bef421b4b2a100f656025c49bccb8e
Daniel <danielrichert62@gmail.com>
2023-07-26 15:05:53 -0500

    adds seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Luke", email: "luke@yahoo.com", image: "http://pluspng.com/img-png/png-user-icon-icons-logos-emojis-users-2400.png")
+
+User.create(name: "Shiela", email: "sheila@yahoo.com", image: "https://ceacopiniones.es/wp-content/uploads/2019/03/user-8.png")
+
+User.create(name: "Horace", email: "horace@yahoo.com", image: "https://acidcow.com/pics/20090812/pics/8/ugly_people_24.jpg")
