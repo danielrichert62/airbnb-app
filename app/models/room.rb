8f6e6fa7c9c8b0070ac5405923e096c9969cb5bb
Daniel <danielrichert62@gmail.com>
2023-07-26 14:21:37 -0500

    adds model associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+  belongs_to :user
+  has_many :room_images
+  has_many :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :rooms
+  has_many :reservations
+  has_many :reviews, through: :reservations
 end
