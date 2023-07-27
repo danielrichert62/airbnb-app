5245f484566d566ab2245d117e32dab1760ac118
Westin Walker <westinwalker1988@gmail.com>
2023-07-27 13:07:53 -0600

    adds reservations new/create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,28 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+    render :new
+  end
+
+  def create
+    @reservation = Reservation.create(
+      user_id: params[:user_id],
+      room_id: params[:room_id],
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      price: params[:price],
+      total: params[:total],
+    )
+    redirect_to "/reservations"
+  end
+
   def index
     @reservations = Reservation.all
     render :index
+  end
+
+  def show
+    @reservation = Reservation.find_by(id: params(:id))
+    render :show
   end
 end
