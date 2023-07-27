3ced1a6d8afe342415f8f7fb93b7e2a6f1b6be42
Westin Walker <westinwalker1988@gmail.com>
2023-07-27 12:05:58 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@
 
 # User.create(name: "Horace", email: "horace@yahoo.com", image: "https://acidcow.com/pics/20090812/pics/8/ugly_people_24.jpg")
 
-# Reservation.create(user_id: 3, room_id: 103, start_date: "07-26-2023", end_date: "08-02-2023", price: 125.00, total: 875.00)
+Reservation.create(user_id: 3, room_id: 103, start_date: "07-26-2023", end_date: "08-02-2023", price: 125.00, total: 875.00)
