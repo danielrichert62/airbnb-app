3484d366a65329f14f9ce89f9af444f299bda091
luke <luke.nadackapadam@gmail.com>
2023-07-27 12:58:05 -0500

    Add index action for rooms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-User.create(name: "Luke", email: "luke@yahoo.com", image: "http://pluspng.com/img-png/png-user-icon-icons-logos-emojis-users-2400.png")
+# User.create(name: "Luke", email: "luke@yahoo.com", image: "http://pluspng.com/img-png/png-user-icon-icons-logos-emojis-users-2400.png")
 
-User.create(name: "Shiela", email: "sheila@yahoo.com", image: "https://ceacopiniones.es/wp-content/uploads/2019/03/user-8.png")
+# User.create(name: "Shiela", email: "sheila@yahoo.com", image: "https://ceacopiniones.es/wp-content/uploads/2019/03/user-8.png")
 
-User.create(name: "Horace", email: "horace@yahoo.com", image: "https://acidcow.com/pics/20090812/pics/8/ugly_people_24.jpg")
+# User.create(name: "Horace", email: "horace@yahoo.com", image: "https://acidcow.com/pics/20090812/pics/8/ugly_people_24.jpg")
+
+# Room.create(
+#   user_id: 1,
+#   address: "5000 Json Ave",
+#   city: "Chicago",
+#   state: "IL",
+#   price: 1000.00,
+#   description: "Very cool!",
+#   home_type: "Mansion",
+#   total_occupancy: 10,
+#   total_bedrooms: 10,
+#   total_bathrooms: 10,
+# )
+
+Room.create(
+  user_id: 2,
+  address: "9000 Sauron St",
+  city: "New York City",
+  state: "NY",
+  price: 5000.0,
+  description: "The best place ever!",
+  home_type: "Tower",
+  total_occupancy: 20,
+  total_bedrooms: 20,
+  total_bathrooms: 20,
+)
+
+Room.create(
+  user_id: 3,
+  address: "123 Forest Rd",
+  city: "Gump",
+  state: "AL",
+  price: 500.0,
+  description: "Visit the house of Forest!",
+  home_type: "House",
+  total_occupancy: 7,
+  total_bedrooms: 7,
+  total_bathrooms: 6,
+)
