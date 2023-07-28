757489a9f38668a2f7cfdacb3479f9c879537325
Daniel <danielrichert62@gmail.com>
2023-07-28 10:58:20 -0500

    Adds update to routes for users and signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to "/"
+      redirect_to "/", notice: "Logged in successfully."
     else
-      redirect_to "/login"
+      redirect_to "/login", notice: "Invalid email or password."
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to "/login"
+    redirect_to "/", notice: "Logged out successfully."
   end
 end
