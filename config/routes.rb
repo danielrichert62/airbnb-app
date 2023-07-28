b5d756148a5922d8722560444fdb173d948a5b45
Yassar Ahmed <yassarahmed17@gmail.com>
2023-07-28 12:45:36 -0500

    resolves conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/users/:id" => "users#show"
-  #get "/users" => "users#index"
+  # get "/users" => "users#show"
   get "/room_images" => "room_images#index"
 end
