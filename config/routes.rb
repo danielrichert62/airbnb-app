57a14df12bb82d0cd1ca9b8a020670c511e69cea
Daniel <danielrichert62@gmail.com>
2023-07-28 08:57:06 -0500

    Adds sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get "/users/:id" => "users#show"
   #get "/users" => "users#index"
   get "/room_images" => "room_images#index"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
